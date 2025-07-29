937f902e24eb43ca1141e2454722a03aa851c875
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-07-29 19:00:08 +0200

    index function|controller

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    //
+    //index method to show the about page
+    public function index(){
+
+        return view('frontend.about');
+    }
 }
