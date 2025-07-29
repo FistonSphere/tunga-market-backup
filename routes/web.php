e1507490e264b2444facb31ee4e430679c66dff1
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-07-29 18:58:41 +0200

    adding about us page route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\frontend\HomeController;
 use App\Http\Controllers\frontend\ProductListingController;
@@ -10,3 +11,4 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/product-discovery-hub', [ProductListingController::class, 'index'])->name('product.discovery');
 Route::get('/product-view', [ProductListingController::class, 'showProduct'])->name('product.view');
 Route::get('/authentication', [AuthController::class, 'showLoginForm'])->name('login');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
