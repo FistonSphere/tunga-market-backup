8c235678e7e08ff17634d1c0131508716c9f2d6f
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-07-29 19:08:32 +0200

    changes in about & contact|controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\AboutController;
+use App\Http\Controllers\frontend\AboutController;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\frontend\ContactController;
 use App\Http\Controllers\frontend\HomeController;
 use App\Http\Controllers\frontend\ProductListingController;
 use Illuminate\Support\Facades\Route;
@@ -12,3 +13,4 @@ Route::get('/product-discovery-hub', [ProductListingController::class, 'index'])
 Route::get('/product-view', [ProductListingController::class, 'showProduct'])->name('product.view');
 Route::get('/authentication', [AuthController::class, 'showLoginForm'])->name('login');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
