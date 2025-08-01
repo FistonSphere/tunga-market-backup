a01f05ab1d29f680f9e1c775c77f9c9ff0872a4f
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-08-01 20:49:41 +0200

    changes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -39,7 +39,7 @@ class Product extends Model
     }
     public function tags()
     {
-        return $this->belongsToMany(Tag::class, 'product_tag');
+        return $this->belongsToMany(ProductTag::class, 'product_tag');
     }
     public function attributes()
     {
