0ecc95cbc50978611993c73bb78b96e1e83383ac
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-08-01 13:43:33 +0200

    changes

--- app/Http/Controllers/frontend/HelpCenterController.php
+++ app/Http/Controllers/frontend/HelpCenterController.php
@@ -10,6 +10,6 @@ class HelpCenterController extends Controller
    public function index()
    {
        // Logic to retrieve and display help center information
-       return view('frontend.help'); // Adjust the view name as necessary
+       return view('frontend.help-center'); // Adjust the view name as necessary
    }
 }
