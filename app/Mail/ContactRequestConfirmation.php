c3ffc08a0f126cb10ca60b3977ab462ac1562d87
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-08-05 21:03:58 +0200

    adding mail file for sending contact requests

--- app/Mail/ContactRequestConfirmation.php
+++ app/Mail/ContactRequestConfirmation.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\ContactRequest;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -13,41 +14,16 @@ class ContactRequestConfirmation extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct()
+    public $contact;
+
+    public function __construct(ContactRequest $contact)
     {
-        //
+        $this->contact = $contact;
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            subject: 'Contact Request Confirmation',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->subject('We Received Your Inquiry')
+            ->view('emails.auth.contact-user');
     }
 }
--- app/Mail/ContactRequestNotification.php
+++ app/Mail/ContactRequestNotification.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\ContactRequest;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -13,41 +14,16 @@ class ContactRequestNotification extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct()
+    public $contact;
+
+    public function __construct(ContactRequest $contact)
     {
-        //
+        $this->contact = $contact;
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            subject: 'Contact Request Notification',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->subject('New Contact Request Received')
+            ->view('emails.auth.contact-admin');
     }
 }
