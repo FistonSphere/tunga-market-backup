adad88204866521e67bb182c3b2471a6775013e5
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-08-10 21:39:12 +0200

    changes

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -3,14 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Wishlist extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'user_id',
         'product_id',
     ];
-
+  protected $dates = ['deleted_at'];
     public function user()
     {
         return $this->belongsTo(User::class);
