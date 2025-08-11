ea2549a98a99901c1f9b62970c875f82159f9c66
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-08-11 17:35:24 +0200

    adding cart model

---
+++ app/Models/Cart.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Cart extends Model
+{
+      use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity',
+        'price',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    // Relationships
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
