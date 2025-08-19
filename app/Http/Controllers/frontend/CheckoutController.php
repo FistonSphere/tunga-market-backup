c6749a88cef708cf01bbc461324f13e4a47483b6
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-08-19 23:15:39 +0200

    adding index function in controller in checkout

--- app/Http/Controllers/frontend/CheckoutController.php
+++ app/Http/Controllers/frontend/CheckoutController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheckoutController extends Controller
 {
     public function index()
     {
-        // Logic to retrieve and display checkout information
+        $cartItems = Cart::with('product')
+            ->where('user_id', Auth::id())
+            ->get();
+
+        if ($cartItems->isEmpty()) {
+            return redirect()->route('cart.index')->with('error', 'Your cart is empty!');
+        }
         return view('frontend.checkout'); // Adjust the view name as necessary
     }
 }
