cc27a0d37e8c20405580374b93315c07114efe5a
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-08-22 21:08:00 +0200

    changes

--- app/Models/ShippingAddress.php
+++ app/Models/ShippingAddress.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ShippingAddress extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'company',
+        'address_line1',
+        'address_line2',
+        'city',
+        'state',
+        'postal_code',
+        'country',
+        'phone',
+        'is_default',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
+
