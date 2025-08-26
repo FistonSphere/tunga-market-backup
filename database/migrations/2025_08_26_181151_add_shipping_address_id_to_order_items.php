44a5cc839599863538f335050a06b62a31717b34
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-08-26 18:15:45 +0200

    changes

--- database/migrations/2025_08_26_181151_add_shipping_address_id_to_order_items.php
+++ database/migrations/2025_08_26_181151_add_shipping_address_id_to_order_items.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('order_items', function (Blueprint $table) {
-           $table->unsignedBigInteger('shipping_address_id')->nullable()->after('order_id');
+        Schema::table('orders', function (Blueprint $table) {
+           $table->unsignedBigInteger('shipping_address_id')->nullable()->after('status');
            $table->foreign('shipping_address_id')->references('id')->on('shipping_addresses')->onDelete('set null');
             
         });
@@ -23,7 +23,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('order_items', function (Blueprint $table) {
+        Schema::table('orders', function (Blueprint $table) {
             $table->dropForeign(['shipping_address_id']);
             $table->dropColumn('shipping_address_id');
         });
