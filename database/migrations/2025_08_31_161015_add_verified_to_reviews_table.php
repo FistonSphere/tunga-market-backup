7e861a26f2a1746398356cac644cb27ade80a36b
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-08-31 16:12:02 +0200

    changes

--- database/migrations/2025_08_31_161015_add_verified_to_reviews_table.php
+++ database/migrations/2025_08_31_161015_add_verified_to_reviews_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('reviews', function (Blueprint $table) {
-            //
+            $table->boolean('verified')->default(false)->after('rating');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('reviews', function (Blueprint $table) {
-            //
+            $table->dropColumn('verified');
         });
     }
 };
