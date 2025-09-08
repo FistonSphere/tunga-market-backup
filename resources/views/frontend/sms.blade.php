409fe805beede58e90d19dc2eb796543235e953a
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-09-08 22:01:49 +0200

    changes

--- resources/views/frontend/sms.blade.php
+++ resources/views/frontend/sms.blade.php
@@ -14,7 +14,7 @@
 
         @if (session('success'))
             <div class="bg-green-100 text-green-700 p-4 rounded mb-4">
-                {{ session('success') }}
+                <p>Message sent successfully</p>
             </div>
         @endif
 
