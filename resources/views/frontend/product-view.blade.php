fcdd4a5e974a965d1117905c6cf0253e025a3213
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-09-08 11:06:54 +0200

    changes

--- resources/views/frontend/product-view.blade.php
+++ resources/views/frontend/product-view.blade.php
@@ -499,16 +499,16 @@
                                         class="star h-10 w-10 text-gray-300 hover:text-yellow-400 transition duration-200 cursor-pointer"
                                         fill="currentColor" viewBox="0 0 20 20">
                                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.178
-                                    3.63a1 1 0 00.95.69h3.813c.969 0
-                                    1.371 1.24.588 1.81l-3.087
-                                    2.243a1 1 0 00-.364 1.118l1.178
-                                    3.63c.3.921-.755 1.688-1.54
-                                    1.118l-3.087-2.243a1 1 0
-                                    00-1.176 0l-3.087
-                                    2.243c-.784.57-1.838-.197-1.539-1.118l1.178-3.63a1 1 0
-                                    00-.364-1.118L2.42
-                                    9.057c-.783-.57-.38-1.81.588-1.81h3.813a1 1 0
-                                    00.951-.69l1.178-3.63z" />
+                                        3.63a1 1 0 00.95.69h3.813c.969 0
+                                        1.371 1.24.588 1.81l-3.087
+                                        2.243a1 1 0 00-.364 1.118l1.178
+                                        3.63c.3.921-.755 1.688-1.54
+                                        1.118l-3.087-2.243a1 1 0
+                                        00-1.176 0l-3.087
+                                        2.243c-.784.57-1.838-.197-1.539-1.118l1.178-3.63a1 1 0
+                                        00-.364-1.118L2.42
+                                        9.057c-.783-.57-.38-1.81.588-1.81h3.813a1 1 0
+                                        00.951-.69l1.178-3.63z" />
                                     </svg>
                                 @endfor
                             </div>
@@ -540,11 +540,12 @@
                         <!-- Submit Button -->
                         <div class="md:w-1/4 flex md:justify-end">
                             <button type="submit"
-                                class="bg-primary hover:bg-primary/90 text-white font-semibold py-3 px-8 
-                           rounded-xl shadow-md hover:shadow-lg transition transform hover:scale-105">
+                                class="bg-primary hover:bg-primary/90 text-white font-medium py-2 px-5 
+               rounded-lg shadow-sm hover:shadow-md transition transform hover:scale-105 text-sm md:text-base">
                                 Submit Review
                             </button>
                         </div>
+
                     </form>
                 </div>
             </div>
