68040ef3d7b417ffb53aa2a081a5742b91ae3cf7
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-09-17 09:23:09 +0200

    changes

--- resources/views/frontend/orders/show.blade.php
+++ resources/views/frontend/orders/show.blade.php
@@ -17,7 +17,7 @@
                 <svg class="w-4 h-4 text-secondary-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
                 </svg>
-                <span class="text-primary font-semibold" id="breadcrumb-order-number">Order {{ $orderNo }}</span>
+                <span class="text-primary font-semibold" id="breadcrumb-order-number">Order #{{ $orderNo }}</span>
             </nav>
         </div>
     </section>
@@ -47,13 +47,13 @@
                                 </svg>
                                 <span>Ordered on <span id="header-order-date" class="font-semibold">{{$order->created_at->format('M d, Y')}}</span></span>
                             </div>
-                            <div class="flex items-center space-x-2 mt-2 sm:mt-0">
-                                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <div class="flex items-center space-x-2 mt-2 sm:mt-0" style="margin-left: 20px;">
+                                {{-- <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1" />
-                                </svg>
+                                </svg> --}}
                                 <span>Total: <span id="header-order-total"
-                                        class="font-bold text-accent text-lg">$2,847.50</span></span>
+                                        class="font-bold text-accent text-lg">{{ number_format($order->items->first()->price * $order->items->first()->quantity)}} Rwf</span></span>
                             </div>
                         </div>
                     </div>
