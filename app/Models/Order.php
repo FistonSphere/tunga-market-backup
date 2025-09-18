306d8f5246adfb0c7d401a71a2be7bd2dac3e517
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-09-18 16:05:26 +0200

    adding PaymentsTableSeeder

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -32,4 +32,9 @@ class Order extends Model
     {
         return $this->belongsTo(ShippingAddress::class);
     }
+
+    public function payment()
+{
+    return $this->hasOne(Payment::class);
+}
 }
