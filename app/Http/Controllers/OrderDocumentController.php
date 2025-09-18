47aa4939fcb88088f3bcf0f39ce240d10d44df0c
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-09-18 17:36:04 +0200

    changes

--- app/Http/Controllers/OrderDocumentController.php
+++ app/Http/Controllers/OrderDocumentController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
+use PDF;
 
 class OrderDocumentController extends Controller
 {
-    //
+    public function invoice(Order $order)
+    {
+        $pdf = PDF::loadView('documents.invoice', compact('order'));
+        return $pdf->stream("invoice-{$order->id}.pdf");
+    }
+
+    public function downloadInvoice(Order $order)
+    {
+        $pdf = PDF::loadView('documents.invoice', compact('order'));
+        return $pdf->download("invoice-{$order->id}.pdf");
+    }
+
+    public function receipt(Order $order)
+    {
+        $pdf = PDF::loadView('documents.receipt', compact('order'));
+        return $pdf->stream("receipt-{$order->id}.pdf");
+    }
+
+    public function downloadReceipt(Order $order)
+    {
+        $pdf = PDF::loadView('documents.receipt', compact('order'));
+        return $pdf->download("receipt-{$order->id}.pdf");
+    }
 }
+
