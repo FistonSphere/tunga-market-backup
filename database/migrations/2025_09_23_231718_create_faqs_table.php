0a9bd05400de77a2a81c707791258d1d17c201e3
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-09-24 17:51:11 +0200

    changes

--- database/migrations/2025_09_23_231718_create_faqs_table.php
+++ database/migrations/2025_09_23_231718_create_faqs_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('faqs', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+        $table->string('question');
+        $table->text('answer');
+        $table->boolean('is_active')->default(true); // allow enabling/disabling FAQ
+        $table->timestamps();
         });
     }
 
