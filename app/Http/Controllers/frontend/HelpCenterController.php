2cb9034db59d52b9295039919504f01d03e03a63
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-09-24 17:59:36 +0200

    changes

--- app/Http/Controllers/frontend/HelpCenterController.php
+++ app/Http/Controllers/frontend/HelpCenterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Faq;
 use Illuminate\Http\Request;
 
 class HelpCenterController extends Controller
@@ -10,6 +11,7 @@ class HelpCenterController extends Controller
    public function index()
    {
        // Logic to retrieve and display help center information
-       return view('frontend.help-center'); // Adjust the view name as necessary
+       $faqs = Faq::where('is_active', true)->orderBy('created_at', 'desc')->get();
+       return view('frontend.help-center', compact('faqs')); // Adjust the view name as necessary
    }
 }
