eaeef16dcebe12063bb0bb4b97681f4a37dfc8db
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-09-25 11:17:01 +0200

    changes

--- app/Http/Controllers/frontend/HelpCenterController.php
+++ app/Http/Controllers/frontend/HelpCenterController.php
@@ -8,14 +8,20 @@ use Illuminate\Http\Request;
 
 class HelpCenterController extends Controller
 {
-   public function index()
-   {
-       $faqs = Faq::where('is_active', true)
-            ->orderBy('category')
-            ->orderBy('topic')
-            ->orderBy('created_at', 'desc')
-            ->get();
+public function index()
+{
+    $faqs = Faq::where('is_active', true)
+        ->orderBy('category')
+        ->orderBy('topic')
+        ->orderBy('created_at', 'desc')
+        ->get()
+        ->groupBy('category') // groups by buyer/seller/platform
+        ->map(function ($categoryGroup) {
+            return $categoryGroup->groupBy('topic'); // then group inside each category by topic
+        });
 
-        return view('frontend.help-center', compact('faqs'));
-   }
+    return view('frontend.help-center', compact('faqs'));
+}
+
+
 }
