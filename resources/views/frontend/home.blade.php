5af38cf79812360747944dd9b1ee193dd551603e
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-01 08:59:37 +0200

    changes

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -422,7 +422,7 @@
         </section>
     @endif
 
-    
+
     <!-- Success Stories Section -->
     <section class="py-16 bg-gradient-to-r from-primary-50 to-accent-50">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
