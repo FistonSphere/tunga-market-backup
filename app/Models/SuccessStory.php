7fe48755ee4f249e7e00162b78803b8af5dff4b7
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-05 12:33:01 +0200

    changes

---
+++ app/Models/SuccessStory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SuccessStory extends Model
+{
+    protected $fillable = [
+        'name',
+        'role',
+        'company',
+        'photo',
+        'testimonial',
+        'highlight_1',
+        'highlight_2',
+        'is_active',
+    ];
+}
