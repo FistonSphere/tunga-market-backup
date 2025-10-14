1e0192b4f58934899e92365d7f24f96cfac3bced
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-14 10:25:38 +0200

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,6 +27,7 @@ class DatabaseSeeder extends Seeder
             FlashDealsTableSeeder::class, // ✅ New
             SuccessStoriesTableSeeder::class, // ✅ New
             TeamMemberSeeder::class, // ✅ New
+            AdvertisementSeeder::class, // ✅ New
 
 
         ]);
