cfdc132e504ddd98ffc821e6b3dc8dda699e621b
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-16 14:46:18 +0200

    changes

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -98,12 +98,6 @@
     </div>
 
     <script src="{{ asset('admin/assets/js/jquery-3.6.0.min.js') }}"></script>
-
-    <script src="{{ asset('admin/assets/js/jquery-3.6.0.min.js') }}"></script>
-
-    <script src="{{ asset('admin/assets/js/jquery-3.6.0.min.js') }}"></script>
-
-    <script src="{{ asset('admin/assets/js/jquery-3.6.0.min.js') }}"></script>
 </body>
 
 </html>
