c321db7b6cd413113e2c8b85113764f06a8516c4
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-17 22:34:32 +0200

    changes

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
-    //
+    public function sendResetLink(Request $request)
+    {
+        // Logic to send password reset link to the user's email
+
+        return view('frontend.auth.reset');
+    }
 }
