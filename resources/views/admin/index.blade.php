924269945945d13925bf6633849b91ed7b268d14
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-18 14:27:11 +0200

    changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,95 +2,112 @@
 
 @section('content')
     <div class="row">
+        <!-- Total Users -->
         <div class="col-lg-3 col-sm-6 col-12">
             <div class="dash-widget">
                 <div class="dash-widgetimg">
-                    <span><img src="assets/img/icons/dash1.svg" alt="img"></span>
+                    <span><img src="{{ asset('assets/img/icons/user.svg') }}" alt="Users"></span>
                 </div>
                 <div class="dash-widgetcontent">
-                    <h5>$<span class="counters" data-count="307144.00">$307,144.00</span></h5>
-                    <h6>Total Purchase Due</h6>
+                    <h5><span class="counters" data-count="{{ $totalUsers }}">{{ $totalUsers }}</span></h5>
+                    <h6>Total Users</h6>
                 </div>
             </div>
         </div>
+
+        <!-- Total Products -->
         <div class="col-lg-3 col-sm-6 col-12">
             <div class="dash-widget dash1">
                 <div class="dash-widgetimg">
-                    <span><img src="assets/img/icons/dash2.svg" alt="img"></span>
+                    <span><img src="{{ asset('assets/img/icons/box.svg') }}" alt="Products"></span>
                 </div>
                 <div class="dash-widgetcontent">
-                    <h5>$<span class="counters" data-count="4385.00">$4,385.00</span></h5>
-                    <h6>Total Sales Due</h6>
+                    <h5><span class="counters" data-count="{{ $totalProducts }}">{{ $totalProducts }}</span></h5>
+                    <h6>Total Products</h6>
                 </div>
             </div>
         </div>
+
+        <!-- Total Orders -->
         <div class="col-lg-3 col-sm-6 col-12">
             <div class="dash-widget dash2">
                 <div class="dash-widgetimg">
-                    <span><img src="assets/img/icons/dash3.svg" alt="img"></span>
+                    <span><img src="{{ asset('assets/img/icons/shopping-cart.svg') }}" alt="Orders"></span>
                 </div>
                 <div class="dash-widgetcontent">
-                    <h5>$<span class="counters" data-count="385656.50">385,656.50</span></h5>
-                    <h6>Total Sale Amount</h6>
+                    <h5><span class="counters" data-count="{{ $totalOrders }}">{{ $totalOrders }}</span></h5>
+                    <h6>Total Orders</h6>
                 </div>
             </div>
         </div>
+
+        <!-- Total Revenue -->
         <div class="col-lg-3 col-sm-6 col-12">
             <div class="dash-widget dash3">
                 <div class="dash-widgetimg">
-                    <span><img src="assets/img/icons/dash4.svg" alt="img"></span>
+                    <span><img src="{{ asset('assets/img/icons/dollar-sign.svg') }}" alt="Revenue"></span>
                 </div>
                 <div class="dash-widgetcontent">
-                    <h5>$<span class="counters" data-count="40000.00">400.00</span></h5>
-                    <h6>Total Sale Amount</h6>
+                    <h5>$<span class="counters"
+                            data-count="{{ $totalRevenue }}">{{ number_format($totalRevenue, 2) }}</span></h5>
+                    <h6>Total Revenue</h6>
                 </div>
             </div>
         </div>
+
+        <!-- Pending Carts -->
         <div class="col-lg-3 col-sm-6 col-12 d-flex">
             <div class="dash-count">
                 <div class="dash-counts">
-                    <h4>100</h4>
-                    <h5>Customers</h5>
+                    <h4>{{ $pendingCarts }}</h4>
+                    <h5>Pending Carts</h5>
                 </div>
                 <div class="dash-imgs">
-                    <i data-feather="user"></i>
+                    <i data-feather="shopping-bag"></i>
                 </div>
             </div>
         </div>
+
+        <!-- Contact Requests -->
         <div class="col-lg-3 col-sm-6 col-12 d-flex">
             <div class="dash-count das1">
                 <div class="dash-counts">
-                    <h4>100</h4>
-                    <h5>Suppliers</h5>
+                    <h4>{{ $contactRequests }}</h4>
+                    <h5>Contact Requests</h5>
                 </div>
                 <div class="dash-imgs">
-                    <i data-feather="user-check"></i>
+                    <i data-feather="mail"></i>
                 </div>
             </div>
         </div>
+
+        <!-- Activity Logs -->
         <div class="col-lg-3 col-sm-6 col-12 d-flex">
             <div class="dash-count das2">
                 <div class="dash-counts">
-                    <h4>100</h4>
-                    <h5>Purchase Invoice</h5>
+                    <h4>{{ $activityLogs }}</h4>
+                    <h5>Activity Logs</h5>
                 </div>
                 <div class="dash-imgs">
-                    <i data-feather="file-text"></i>
+                    <i data-feather="activity"></i>
                 </div>
             </div>
         </div>
+
+        <!-- Product Views Today -->
         <div class="col-lg-3 col-sm-6 col-12 d-flex">
             <div class="dash-count das3">
                 <div class="dash-counts">
-                    <h4>105</h4>
-                    <h5>Sales Invoice</h5>
+                    <h4>{{ $productViewsToday }}</h4>
+                    <h5>Views Today</h5>
                 </div>
                 <div class="dash-imgs">
-                    <i data-feather="file"></i>
+                    <i data-feather="eye"></i>
                 </div>
             </div>
         </div>
     </div>
+
 
     <div class="row">
         <div class="col-lg-7 col-sm-12 col-12 d-flex">
