4c6ce33008c9b5acd82558663bd5b4669b3ecbae
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-18 20:38:11 +0200

    changes

--- app/Http/Controllers/backend/HomeController.php
+++ app/Http/Controllers/backend/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
     {
 
         $recentProducts = Product::latest()->take(5)->get();
-          $sales = Order::selectRaw('DATE(created_at) as date, SUM(total) as total')
+        $sales = Order::selectRaw('DATE(created_at) as date, SUM(total) as total')
         ->where('status', 'Delivered')  // Only include delivered orders
         ->whereBetween('created_at', [now()->subDays(6)->startOfDay(), now()->endOfDay()])
         ->groupBy('date')
@@ -39,6 +39,8 @@ class HomeController extends Controller
 
     $salesDates = $dateRange->keys()->map(fn($d) => Carbon::parse($d)->format('M d'))->toArray();
     $salesTotals = $dateRange->values()->toArray();
+
+    
       return view('admin.index', [
         'totalUsers'         => User::count(),
         'totalProducts'      => Product::count(),
