19503964b286c05f758fe2cdf1b5667dc2bf7ae2
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-26 18:15:53 +0200

    changes

--- routes/web.php
+++ routes/web.php
@@ -235,6 +235,7 @@ Route::get('/create',  'create')->name('products.admin.create');
 Route::post('/products', 'store')->name('products.store');
 Route::get('/filter', 'filter')->name('products.filter');
 Route::get('/print-pdf', 'printPDF')->name('admin.products.printPDF');
+Route::get('/save-pdf', 'savePDF')->name('admin.products.savePDF');
 
 });
 
