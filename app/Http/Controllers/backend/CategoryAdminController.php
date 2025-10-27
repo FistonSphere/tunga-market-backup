663783e0147f3d46335b9e8edd7a6dce6f94cda1
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-27 10:13:51 +0200

    changes

--- app/Http/Controllers/backend/CategoryAdminController.php
+++ app/Http/Controllers/backend/CategoryAdminController.php
@@ -13,4 +13,20 @@ class CategoryAdminController extends Controller
     $categories = Category::all();
     return view('admin.category.index', compact('categories'));
    }
+
+  public function destroy($id)
+{
+    $product = Category::findOrFail($id);
+
+    // Optionally delete image files if you store them locally
+    if ($product->main_image && file_exists(public_path('storage/' . $product->main_image))) {
+        unlink(public_path('storage/' . $product->main_image));
+    }
+
+    
+
+    $product->delete();
+
+    return redirect()->route('admin.product.listing')->with('success', 'Product deleted successfully!');
+}
 }
