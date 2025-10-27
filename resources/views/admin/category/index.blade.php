872e843ebc13f1b26b0690b8c2c1cb28f29acfb4
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-27 09:09:47 +0200

    changes

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -25,7 +25,8 @@
                         </a>
                     </div>
                     <div class="search-input">
-                        <a class="btn btn-searchset"><img src="{{ asset('admin/assets/img/icons/search-white.svg') }}" alt="img" /></a>
+                        <a class="btn btn-searchset"><img src="{{ asset('admin/assets/img/icons/search-white.svg') }}"
+                                alt="img" /></a>
                     </div>
                 </div>
                 <div class="wordset">
@@ -59,39 +60,51 @@
                                 </label>
                             </th>
                             <th>Category name</th>
-                            <th>Category Code</th>
+                            <th>Category Slug</th>
                             <th>Description</th>
-                            <th>Created By</th>
+                            <th>Status</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>
-                                <label class="checkboxs">
-                                    <input type="checkbox" />
-                                    <span class="checkmarks"></span>
-                                </label>
-                            </td>
-                            <td class="productimgname">
-                                <a href="javascript:void(0);" class="product-img">
-                                    <img src="assets/img/product/noimage.png" alt="product" />
-                                </a>
-                                <a href="javascript:void(0);">Computers</a>
-                            </td>
-                            <td>CT001</td>
-                            <td>Computers Description</td>
-                            <td>Admin</td>
-                            <td>
-                                <a class="me-3" href="editcategory.html">
-                                    <img src="assets/img/icons/edit.svg" alt="img" />
-                                </a>
-                                <a class="me-3 confirm-text" href="javascript:void(0);">
-                                    <img src="assets/img/icons/delete.svg" alt="img" />
-                                </a>
-                            </td>
-                        </tr>
-                       
+
+                        @foreach ($categories as $category)
+                            <tr>
+                                <td>
+                                    <label class="checkboxs">
+                                        <input type="checkbox" />
+                                        <span class="checkmarks"></span>
+                                    </label>
+                                </td>
+                                <td class="productimgname">
+                                    <a href="javascript:void(0);" class="product-img">
+                                        <img src="{{ $category->thumbnail ?? asset('assets/images/no-image.png') }}" style="border-radius:8px" alt="{{ $category->name }}" />
+                                    </a>
+                                    <a href="javascript:void(0);">{{ $category->name }}</a>
+                                </td>
+                                <td>{{ $category->slug }}</td>
+                                <td>{{ $category->description ?? '-' }}</td>
+                                <td>
+                                @if ($category->is_active == 1)
+                                Active
+
+                                @else
+                                In Active
+                                @endif
+
+                                </td>
+                                <td>
+                                    <a class="me-3" href="editcategory.html">
+                                        <img src="{{asset('admin/assets/img/icons/edit.svg')}}" alt="img" />
+                                    </a>
+                                    <a class="me-3 confirm-text" href="javascript:void(0);">
+                                        <img src="{{asset('admin/assets/img/icons/delete.svg')}}" alt="img" />
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+
+
 
                     </tbody>
                 </table>
