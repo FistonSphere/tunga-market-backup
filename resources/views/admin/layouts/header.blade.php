7bc2d67755d4c01b0a1c7f2fe3fbb4f22aa752a5
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-30 12:57:28 +0200

    changes

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -29,6 +29,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
 
+
 </head>
 
 <body>
