223135845b181ee13e946dae1a9a8dd621d7b2f7
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-30 22:01:14 +0200

    changes

--- app/Http/Controllers/backend/BrandController.php
+++ app/Http/Controllers/backend/BrandController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
    public function index(){
 
-    return view('admin')
+    $brands=Brand::all();
+    return view('admin.brand.index', compact('brands'));
    }
 }
