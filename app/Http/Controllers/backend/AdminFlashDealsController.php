da6c740bc20df44cd0628c6021ea50fd5271ad94
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-10-31 11:41:36 +0200

    changes

--- app/Http/Controllers/backend/AdminFlashDealsController.php
+++ app/Http/Controllers/backend/AdminFlashDealsController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\FlashDeal;
 use Illuminate\Http\Request;
 
 class AdminFlashDealsController extends Controller
 {
-    //
+   public function index(){
+
+    $flashDeals= FlashDeal::all();
+    return view('admin.flash-deals.index', compact('flashDeals'));
+    
+   }
 }
