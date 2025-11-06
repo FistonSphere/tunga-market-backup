9c0f796ccf148169f9874a1d5d658d5300a4da8e
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-11-06 09:19:50 +0200

    changes

--- resources/views/admin/orders/items.blade.php
+++ resources/views/admin/orders/items.blade.php
@@ -293,25 +293,28 @@
             <div class="order-summary-card">
                 <!-- ===== Order Actions on Top ===== -->
                 <div class="order-actions">
-                    @if ($order->payment?->status == 'pending')
-                        <button class="btn-primary">
+                    @if ($order->payment?->status === 'paid')
+                        <button class="btn-primary" type="button">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-cash-stack" viewBox="0 0 16 16">
                                 <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
                                 <path
                                     d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2z" />
-                            </svg> Mark as Paid
+                            </svg>
+                            Mark as Unpaid
                         </button>
                     @else
-                        <button class="btn-primary">
+                        <button class="btn-primary" type="button">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-cash-stack" viewBox="0 0 16 16">
                                 <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
                                 <path
                                     d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2z" />
-                            </svg> Mark as Unpaid
+                            </svg>
+                            Mark as Paid
                         </button>
                     @endif
+
 
                     <button class="btn-outline">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -429,7 +432,7 @@
                         <div>
                             <p class="label">Payment</p>
                             <span class="badge payment-{{ $order->payment ? 'paid' : 'unpaid' }}">
-                                {{ $order->payment ? 'Paid' : 'Unpaid' }}
+                                {{ $order->payment->status ?? 'unpaid' }}
                             </span>
                         </div>
                     </div>
