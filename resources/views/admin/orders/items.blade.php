ea3c3f03cf7abfe6d5fc16d0e0c8cb5f7524374d
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-11-06 10:06:47 +0200

    changes

--- resources/views/admin/orders/items.blade.php
+++ resources/views/admin/orders/items.blade.php
@@ -377,19 +377,24 @@
                         style="background: none; border: none; cursor: pointer; padding: 0; display: flex; align-items: center;"
                         title="Copy Invoice Number">
                         <!-- Copy Icon -->
-                        <svg id="copyInvoiceIcon" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                            fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+                        <svg id="copyInvoiceIcon" style="color:#fd5e0e" xmlns="http://www.w3.org/2000/svg" width="16"
+                            height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
                             <path
-                                d="M10 1.5v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5z" />
+                                d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
                             <path
-                                d="M9.5 0a1.5 1.5 0 0 1 1.5 1.5V2h1a2 2 0 0 1 2 2V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h1v-.5A1.5 1.5 0 0 1 6.5 0h3z" />
+                                d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z" />
                         </svg>
 
                         <!-- Check Icon -->
-                        <svg id="checkInvoiceIcon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="green"
-                            class="bi bi-check-circle" viewBox="0 0 16 16" style="display: none;">
+                        <svg id="checkInvoiceIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            fill="currentColor" class="bi bi-clipboard-check" viewBox="0 0 16 16"
+                            style="display: none; color: #fd5e0e;">
+                            <path fill-rule="evenodd"
+                                d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0" />
                             <path
-                                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm3.354-8.646a.5.5 0 0 1 0 .707l-4 4a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7 10.293l3.646-3.647a.5.5 0 0 1 .708 0z" />
+                                d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
+                            <path
+                                d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z" />
                         </svg>
                     </button>
 
