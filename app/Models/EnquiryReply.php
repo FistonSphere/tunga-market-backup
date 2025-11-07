db166ed7d1b20769bd34f228687218c395113015
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-11-07 12:53:54 +0200

    changes

--- app/Models/EnquiryReply.php
+++ app/Models/EnquiryReply.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class EnquiryReply extends Model
 {
-    //
+    protected $fillable = ['enquiry_id', 'user_id', 'message'];
+
+    public function enquiry()
+    {
+        return $this->belongsTo(Enquiry::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
