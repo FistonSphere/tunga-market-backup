76d6b684d3badfedaa8d10deb60911188d03a63b
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-11-10 22:50:48 +0200

    changes

--- resources/views/admin/support/contact-request.blade.php
+++ resources/views/admin/support/contact-request.blade.php
@@ -413,8 +413,17 @@
                         <option value="high" {{ request('priority') == 'high' ? 'selected' : '' }}>High</option>
                     </select>
 
-                    <button class="btn btn-primary btn-sm"><i class="bi bi-funnel"></i> Filter</button>
-                    <a href="/admin/support/contact-requests" onclick="window.history.back()" class="btn btn-primary btn-sm"><i class="bi bi-funnel"></i> Reset</a>
+                    <button class="btn btn-primary btn-sm"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
+                            <path
+                                d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z" />
+                        </svg> Filter</button>
+                    <a href="/admin/support/contact-requests" onclick="window.history.back()"
+                        class="btn btn-primary btn-sm"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            fill="currentColor" class="bi bi-repeat" viewBox="0 0 16 16">
+                            <path
+                                d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z" />
+                        </svg> Reset</a>
                 </form>
 
             </div>
@@ -461,12 +470,12 @@
                                             class="bi bi-eye" viewBox="0 0 16 16">
                                             <path
                                                 d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 
-                                                                                                                    4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 
-                                                                                                                    1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 
-                                                                                                                    1.172 8z" />
+                                                                                                                                    4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 
+                                                                                                                                    1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 
+                                                                                                                                    1.172 8z" />
                                             <path
                                                 d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 
-                                                                                                                    8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                                                                                                                                    8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
                                         </svg>
                                     </button>
 
