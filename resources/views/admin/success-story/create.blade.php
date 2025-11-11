d6d4a8b9b6efb97ab0e01e97354c92f97e1cb19a
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-11-11 18:17:12 +0200

    changes

--- resources/views/admin/success-story/create.blade.php
+++ resources/views/admin/success-story/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <style>
         /* Layout */
-        .brand-create-container {
+        .Story-create-container {
             background: #fff;
             border-radius: 16px;
             padding: 25px;
@@ -165,9 +165,9 @@
             }
         }
     </style>
-    <div class="brand-create-container">
+    <div class="Story-create-container">
         <div class="header-bar">
-            <h2><i class="bi bi-bag-plus"></i> Create New Brand</h2>
+            <h2><i class="bi bi-bag-plus"></i> Create New Story</h2>
             <div class="action-buttons">
                 <a href="{{ route('admin.successStories.index') }}" class="btn-cancel">Cancel</a>
                 <button type="submit" form="storyForm" class="btn-save">
@@ -186,31 +186,47 @@
             <div class="form-layout">
                 <!-- Left Side -->
                 <div class="form-left">
-                    <!-- Brand Name -->
+                    <!-- Story Name -->
                     <div class="form-card">
-                        <label for="name">Brand Name <span class="required">*</span></label>
-                        <input type="text" id="name" name="name" placeholder="Enter brand name..." required>
+                        <label for="name">Person Name <span class="required">*</span></label>
+                        <input type="text" id="name" name="name" placeholder="Enter person name..." required>
+                    </div>
+                    <!-- company name -->
+                    <div class="form-card">
+                        <label for="name">Company Name <span class="required"></span></label>
+                        <input type="text" id="company" name="company" placeholder="Enter company name..." required>
+                    </div>
+                    <!-- role -->
+                    <div class="form-card">
+                        <label for="name">Role/Position <span class="required"></span></label>
+                        <input type="text" id="role" name="role" placeholder="Enter role or position..." required>
                     </div>
 
                     <!-- Description -->
                     <div class="form-card">
-                        <label for="description">Description</label>
-                        <textarea id="description" name="description" rows="5"
-                            placeholder="Write short description..."></textarea>
+                        <label for="description">Testimonal Message</label>
+                        <textarea id="testimonial" name="testimonial" rows="5"
+                            placeholder="Write testimonial..."></textarea>
                     </div>
                 </div>
 
                 <!-- Right Side -->
                 <div class="form-right">
                     <div class="upload-card">
-                        <label for="logo">Brand Logo</label>
-                        <div class="upload-box" onclick="document.getElementById('logo').click()">
-                            <i class="bi bi-cloud-upload"></i>
-                            <p>Click or drag to upload logo</p>
-                            <input type="file" id="logo" name="logo" accept="image/*" onchange="previewLogo(event)">
+                        <label for="photo">Story photo</label>
+                        <div class="upload-box" onclick="document.getElementById('photo').click()">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-cloud-upload" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383" />
+                                <path fill-rule="evenodd"
+                                    d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708z" />
+                            </svg>
+                            <p>Click or drag to upload photo</p>
+                            <input type="file" id="photo" name="photo" accept="image/*" onchange="previewLogo(event)">
                         </div>
                         <div class="preview-box" id="logoPreview">
-                            <img src="{{ asset('assets/images/no-image.png') }}" alt="Logo Preview">
+                            <img src="{{ asset('assets/images/no-image.png') }}" alt="photo Preview">
                         </div>
                     </div>
                 </div>
