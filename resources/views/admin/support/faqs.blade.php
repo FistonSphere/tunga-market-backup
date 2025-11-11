4aaee4404f6a2b2a5b77f52a83b9fde09cc335fe
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-11-11 13:03:02 +0200

    changes

--- resources/views/admin/support/faqs.blade.php
+++ resources/views/admin/support/faqs.blade.php
@@ -155,7 +155,6 @@
 
         /* Button Styling */
         .btn {
-            padding: 0.8rem 2rem;
             font-size: 1rem;
             font-weight: 600;
             border-radius: 6px;
@@ -246,6 +245,7 @@
             border-radius: .3rem;
             outline: 0;
         }
+
     </style>
 
     <div class="faq-container">
@@ -294,6 +294,16 @@
                                 </span>
                             </td>
                             <td>
+                                <button class="btn btn-sm btn-outline-success view-btn" data-faq-id="{{ $faq->id }}"
+                                    type="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-eye" viewBox="0 0 16 16">
+                                        <path
+                                            d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                                        <path
+                                            d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                                    </svg>
+                                </button>
                                 <button class="btn btn-sm btn-outline-primary edit-btn" data-bs-toggle="modal"
                                     data-bs-target="#editFaqModal" data-faq-id="{{ $faq->id }}"
                                     data-category="{{ $faq->category }}" data-topic="{{ $faq->topic }}"
@@ -307,6 +317,7 @@
                                             d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
                                     </svg>
                                 </button>
+
                                 <button class="btn btn-sm btn-outline-danger delete-btn" data-faq-id="{{ $faq->id }}"
                                     type="button">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
