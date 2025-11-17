3b8cceebea58bf75b5e340e6b8cecfda9a4bf480
Fiston Sano <92255846+FistonSphere@users.noreply.github.com>
2025-11-17 10:08:37 +0200

    changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -87,4 +87,11 @@ public function cartItems()
     return $this->hasMany(Cart::class);
 }
 
+protected static function booted()
+{
+    static::updating(function ($user) {
+        $user->last_active_at = now();
+    });
+}
+
 }
